97e46c7b8d7440d61810c54a72fa4938419297c3
Jon Beck <jonnydbeck@gmail.com>
2023-01-30 19:36:09 -0500

    Profile Model and Index Updated

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -11,6 +11,10 @@ const profileSchema = new Schema (
             type: String,
             required: true
         },
+        email: {
+            type: String,
+            required: true
+        },
         tracks: {
             type: [Track],
             required: false
---
+++ server/models/index.js
@@ -0,0 +1,9 @@
+const Profile = require('./Profile');
+const Track = require('./Track');
+const Exercise = require('./Exercise');
+
+module.exports {
+    Profile,
+    Track,
+    Exercise
+}
