2a08177819597aae0fd57cdcf3a597c965a504a7
MHKhalil1 <khalilmahmoud2333@gmail.com>
2023-02-06 18:57:56 -0500

    Export for UserLogin

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,13 @@
 import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+mutation loginUser($email: String!, $password: String!){
+    login(email: $email, password: $password){
+        token
+        user{
+            _id
+        }
+    }
+}
+`;
+
