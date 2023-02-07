448e8b7339e79fd83476fa9a6eecd6c449064804
MHKhalil1 <khalilmahmoud2333@gmail.com>
2023-02-06 19:18:43 -0500

    Export for Adding Users

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -11,3 +11,14 @@ mutation loginUser($email: String!, $password: String!){
 }
 `;
 
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!){
+    addUser(username: $username, email: $email, password: $password){
+        token
+        user{
+            _id
+            username
+        }
+    }
+  }
+`;
