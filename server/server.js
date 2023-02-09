37c06efdfb8a6453ba42cba40ee320fac7974166
Jon Beck <jonnydbeck@gmail.com>
2023-02-08 21:54:26 -0500

    Heroku Build

--- server/server.js
+++ server/server.js
@@ -23,8 +23,20 @@ app.use("/api/user", userRoutes);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
 
+if (process.env.NODE_ENV = 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "../client"))
+    res.sendFile(path.join(__dirname, "../client/build/index.html"))
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"))
+})
+
+app.get('/signup', (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"))
 })
 
 const startApolloServer = async(typeDefs, resolvers) => {
